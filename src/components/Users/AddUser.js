ac2eafcc493be3dc2106f59010b1e1ce250d9062
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-27 15:38:34 +0100

    added UsersList component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -36,9 +36,19 @@ const AddUser = (props) => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username</label>
-                <input id='username' type='text' value={enteredUsername} onChange={usernameChangeHandler} />
+                <input 
+                    id='username' 
+                    type='text' 
+                    value={enteredUsername} 
+                    onChange={usernameChangeHandler}
+                />
                 <label htmlFor='age'>Age (Years)</label>
-                <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler} />
+                <input 
+                    id='age' 
+                    type='number' 
+                    value={enteredAge} 
+                    onChange={ageChangeHandler} 
+                />
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
